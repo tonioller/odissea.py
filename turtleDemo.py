5864603214f0c30936609ac82d0c857f9210bfde
tonioller <toni.oller@gmail.com>
2022-06-30 00:46:24 +0200

    Create turtleDemo.py

---
+++ turtleDemo.py
@@ -0,0 +1,19 @@
+#Importamos la librería turtle
+import turtle
+#Procedimiento para crear el cuadrado
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(135)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(135)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(30)
+
+turtle.done()
