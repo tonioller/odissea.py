3227832978c45fe80da946f6ed5c61021f6dd294
tonioller <toni.oller@gmail.com>
2022-06-30 00:57:23 +0200

    Update turtleDemo.py

--- turtleDemo.py
+++ turtleDemo.py
@@ -1,6 +1,6 @@
-#Importamos la librería turtle
 import turtle
-#Procedimiento para crear el cuadrado
+
+# https://realpython.com/beginners-guide-python-turtle/
 turtle.left(90)
 turtle.forward(50)
 turtle.right(90)
@@ -15,5 +15,8 @@ turtle.right(90)
 turtle.forward(50)
 turtle.right(90)
 turtle.forward(30)
+turtle.pencolor("red")
+turtle.circle(60)
+turtle.dot(20)
 
 turtle.done()
